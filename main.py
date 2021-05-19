5629b8e08f109a0f0bfa2c07177724b3caed313d
Diego Calderon <>
2021-05-19 00:53:32 +0000

    Nombre

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import turtle
-turtle.home()
+
+#LETRA D
+turtle.up()
+turtle.goto(-800,0)
+turtle.down()
 turtle.shape('turtle')
 turtle.pencolor('red')
 turtle.pensize(10)
@@ -11,8 +15,6 @@ turtle.forward(200)
 turtle.left(40)
 turtle.shape('turtle')
 turtle.pencolor('pink')
-
-#LETRA D
 turtle.forward(200)
 turtle.left(50)
 turtle.shape('turtle')
@@ -27,15 +29,71 @@ turtle.shape('turtle')
 turtle.pencolor('green')
 turtle.forward(480)
 turtle.left(90)
-
+"""
+"""
 #ESPACIO
 turtle.up()
-turtle.goto(500,0)
+turtle.goto(-300,0)
 turtle.down()
 
 #LETRA I
 turtle.left(90)
 turtle.forward(300)
 turtle.up()
-turtle.goto(500,-200)
+turtle.goto(-300,400)
 turtle.down()
+turtle.dot(15,'red')
+turtle.up()
+turtle.goto(-300,0)
+turtle.down()
+
+#ESPACIO
+turtle.up()
+turtle.goto(0,0)
+turtle.down()
+
+#LETRA E
+turtle.shape('turtle')
+turtle.pencolor('red')
+turtle.left(-60)
+turtle.forward(250)
+turtle.left(180)
+turtle.shape('turtle')
+turtle.pencolor('blue')
+turtle.up()
+turtle.forward(250)
+turtle.down()
+turtle.left(-90)
+turtle.forward(250)
+turtle.left(290)
+turtle.shape('turtle')
+turtle.pencolor('orange')
+turtle.forward(220)
+turtle.left(270)
+turtle.shape('turtle')
+turtle.pencolor('purple')
+turtle.forward(220)
+turtle.left(-135)
+turtle.shape('turtle')
+turtle.pencolor('pink')
+turtle.forward(310)
+turtle.left(115)
+turtle.shape('turtle')
+turtle.pencolor('blue')
+turtle.up()
+turtle.forward(250)
+turtle.down()
+
+#ESPACIO
+turtle.up()
+turtle.goto(350,0)
+turtle.down()
+
+#LETRA G
+turtle.shape('turtle')
+turtle.pencolor('yellow')
+turtle.forward(200)
+
+
+
+
